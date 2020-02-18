OdilsonItalis/safe-react

commit b960dbf3cf36774a878239c9fed800b58d08abcd

(fix) linting problem that needed to be addressed manually

src/routes/safe/components/Settings/ManageOwners/AddOwnerModal/screens/ThresholdForm/index.jsx
@@ -21,15 +21,16 @@ import { styles } from './style'
 export const ADD_OWNER_THRESHOLD_NEXT_BTN_TEST_ID = 'add-owner-threshold-next-btn'
 
 type Props = {
-  onClose: () => void,
   classes: Object,
+  meta: any,
+  onClickBack: Function,
+  onClose: () => void,
+  onSubmit: Function,
   owners: List<Owner>,
   threshold: number,
-  onClickBack: Function,
-  onSubmit: Function,
 }
 
-const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSubmit }: Props) => {
+const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSubmit, meta }: Props) => {
   const handleSubmit = values => {
     onSubmit(values)
   }
@@ -71,9 +72,9 @@ const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSub
                             </MenuItem>
                           ))}
                         </SelectField>
-                        {props.meta.error && props.meta.touched && (
+                        {meta.error && meta.touched && (
                           <Paragraph className={classes.errorText} noMargin color="error">
-                            {props.meta.error}
+                            {meta.error}
                           </Paragraph>
                         )}
                       </>

src/routes/safe/components/Settings/ManageOwners/RemoveOwnerModal/screens/ThresholdForm/index.jsx
@@ -21,15 +21,16 @@ import { styles } from './style'
 export const REMOVE_OWNER_THRESHOLD_NEXT_BTN_TEST_ID = 'remove-owner-threshold-next-btn'
 
 type Props = {
-  onClose: () => void,
   classes: Object,
+  meta: any,
+  onClickBack: Function,
+  onClose: () => void,
+  onSubmit: Function,
   owners: List<Owner>,
   threshold: number,
-  onClickBack: Function,
-  onSubmit: Function,
 }
 
-const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSubmit }: Props) => {
+const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSubmit, meta }: Props) => {
   const handleSubmit = values => {
     onSubmit(values)
   }
@@ -75,9 +76,9 @@ const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSub
                               </MenuItem>
                             ))}
                           </SelectField>
-                          {props.meta.error && props.meta.touched && (
+                          {meta.error && meta.touched && (
                             <Paragraph className={classes.errorText} noMargin color="error">
-                              {props.meta.error}
+                              {meta.error}
                             </Paragraph>
                           )}
                         </>

src/components/Spacer/index.jsx
@@ -9,4 +9,6 @@ const style = {
   flexGrow: 1,
 }
 
-export default ({ className }: Props) => <div className={className} style={style} />
+const Spacer = ({ className }: Props) => <div className={className} style={style} />
+
+export default Spacer

src/components/forms/Checkbox/index.jsx
@@ -6,7 +6,6 @@ class GnoCheckbox extends React.PureComponent<CheckoxProps> {
   render() {
     const {
       input: { checked, name, onChange, ...restInput },
-      meta,
       ...rest
     } = this.props
 

src/components/forms/TextField/index.jsx
@@ -23,7 +23,6 @@ class TextField extends React.PureComponent<TextFieldProps> {
     const {
       input: { name, onChange, value, ...restInput },
       meta,
-      render,
       text,
       inputAdornment,
       classes,

src/routes/open/components/ReviewInformation/index.jsx
@@ -14,7 +14,7 @@ import Col from '~/components/layout/Col'
 import Row from '~/components/layout/Row'
 import Paragraph from '~/components/layout/Paragraph'
 import { formatAmount } from '~/logic/tokens/utils/formatAmount'
-import { sm, md, lg, border, background, screenSm } from '~/theme/variables'
+import { sm, lg, border, background, screenSm } from '~/theme/variables'
 import Hairline from '~/components/layout/Hairline'
 import { getWeb3 } from '~/logic/wallets/getWeb3'
 import { FIELD_NAME, FIELD_CONFIRMATIONS, getNumOwnersFrom } from '../fields'

src/routes/safe/components/AddressBook/EllipsisTransactionDetails/index.jsx
@@ -55,7 +55,7 @@ const EllipsisTransactionDetails = ({ knownAddress, address }: EllipsisTransacti
 
   return (
     <ClickAwayListener onClickAway={closeMenuHandler}>
-      <div className={classes.container} onClick={handleClick}>
+      <div role="menu" tabIndex={0} className={classes.container} onClick={handleClick} onKeyDown={handleClick}>
         <MoreHorizIcon />
         <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={closeMenuHandler}>
           <MenuItem onClick={closeMenuHandler} disabled>

src/routes/safe/components/Balances/SendModal/screens/AddressBookInput/index.jsx
@@ -156,13 +156,14 @@ const AddressBookInput = ({
         renderInput={params => (
           <TextField
             {...params}
-            label={!isValidForm ? validationText : 'Recipient'}
+            // eslint-disable-next-line jsx-a11y/no-autofocus
+            autoFocus={!blurred || pristine}
             error={!isValidForm}
             fullWidth
-            autoFocus={!blurred || pristine}
-            variant="filled"
             id="filled-error-helper-text"
+            label={!isValidForm ? validationText : 'Recipient'}
             value={{ address: inputAddValue }}
+            variant="filled"
             onChange={event => {
               setInputTouched(true)
               onAddressInputChanged(event.target.value)

commit 3fcdaec1b959a46a2abaab533d20bd9e9517af87

(Fix) Send form fails to load (#574)
* Fix material-ui/lab version

* Fix breaking changes introduced in commit b960dbf3cf36774a878239c9fed800b58d08abcd

* Fix web3connect version to `1.0.0-beta.25`

src/routes/safe/components/Settings/ManageOwners/AddOwnerModal/screens/ThresholdForm/index.jsx
@@ -22,7 +22,6 @@ export const ADD_OWNER_THRESHOLD_NEXT_BTN_TEST_ID = 'add-owner-threshold-next-bt
 
 type Props = {
   classes: Object,
-  meta: any,
   onClickBack: Function,
   onClose: () => void,
   onSubmit: Function,
@@ -30,7 +29,7 @@ type Props = {
   threshold: number,
 }
 
-const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSubmit, meta }: Props) => {
+const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSubmit }: Props) => {
   const handleSubmit = values => {
     onSubmit(values)
   }
@@ -63,7 +62,7 @@ const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSub
                 <Col xs={2}>
                   <Field
                     name="threshold"
-                    render={props => (
+                    render={(props: any) => (
                       <>
                         <SelectField {...props} disableError>
                           {[...Array(Number(owners.size + 1))].map((x, index) => (
@@ -72,9 +71,9 @@ const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSub
                             </MenuItem>
                           ))}
                         </SelectField>
-                        {meta.error && meta.touched && (
+                        {props.meta.error && props.meta.touched && (
                           <Paragraph className={classes.errorText} noMargin color="error">
-                            {meta.error}
+                            {props.meta.error}
                           </Paragraph>
                         )}
                       </>

src/routes/safe/components/Settings/ManageOwners/RemoveOwnerModal/screens/ThresholdForm/index.jsx
@@ -22,7 +22,6 @@ export const REMOVE_OWNER_THRESHOLD_NEXT_BTN_TEST_ID = 'remove-owner-threshold-n
 
 type Props = {
   classes: Object,
-  meta: any,
   onClickBack: Function,
   onClose: () => void,
   onSubmit: Function,
@@ -30,7 +29,7 @@ type Props = {
   threshold: number,
 }
 
-const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSubmit, meta }: Props) => {
+const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSubmit }: Props) => {
   const handleSubmit = values => {
     onSubmit(values)
   }
@@ -67,7 +66,7 @@ const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSub
                   <Col xs={2}>
                     <Field
                       name="threshold"
-                      render={props => (
+                      render={(props: any) => (
                         <>
                           <SelectField {...props} disableError>
                             {[...Array(Number(numOptions))].map((x, index) => (
@@ -76,9 +75,9 @@ const ThresholdForm = ({ classes, onClose, owners, threshold, onClickBack, onSub
                               </MenuItem>
                             ))}
                           </SelectField>
-                          {meta.error && meta.touched && (
+                          {props.meta.error && props.meta.touched && (
                             <Paragraph className={classes.errorText} noMargin color="error">
-                              {meta.error}
+                              {props.meta.error}
                             </Paragraph>
                           )}
                         </>